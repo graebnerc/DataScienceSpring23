c825ba2bd75f95ab950e5f1801939f0883c2831a
Claudius Graebner-Radkowitsch <claudius@claudius-graebner.com>
2022-03-05 21:55:36 +0100

    Add lecture notes for visualization session

--- R/knit_helper.R
+++ R/knit_helper.R
@@ -1,9 +1,11 @@
-post_dir <- "2022-03-31-obj-types-ii-vectors"
-post_dir <- "2022-03-31-obj-types-iii-adv"
-post_dir <- "2022-03-24-first-steps"
+library(here)
+post_dir <- "post/2022-03-31-obj-types-ii-vectors"
+post_dir <- "post/2022-03-31-obj-types-iii-adv"
+post_dir <- "post/2022-03-24-first-steps"
+post_dir <- "notes/T5-visualization-lecture-notes"
 rmarkdown::render(
-  input = here(paste0("content/post/", post_dir, "/pubdir/index.Rmd")), 
+  input = here(paste0("content/", post_dir, "/pubdir/index.Rmd")), 
   "html_document", output_file="onlinecontent.html")
 rmarkdown::render(
-  input = here(paste0("content/post/", post_dir, "/pubdir/index.Rmd")), 
+  input = here(paste0("content/", post_dir, "/pubdir/index.Rmd")), 
   "pdf_document", output_file="pdfcontent.pdf")
