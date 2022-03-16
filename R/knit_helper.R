862c0b04c4868f9ae4ba9889def00f3c55508f09
Claudius Graebner-Radkowitsch <claudius@claudius-graebner.com>
2022-03-16 18:33:15 +0100

    Finalize draft for data import tutorial

--- R/knit_helper.R
+++ R/knit_helper.R
@@ -1,8 +1,10 @@
+here::i_am("R/knit_helper.R")
 library(here)
 post_dir <- "post/2022-03-31-obj-types-ii-vectors"
 post_dir <- "post/2022-03-31-obj-types-iii-adv"
 post_dir <- "post/2022-03-24-first-steps"
 post_dir <- "notes/T5-visualization-lecture-notes"
+post_dir <- "post/importing-exporting-data"
 rmarkdown::render(
   input = here(paste0("content/", post_dir, "/pubdir/index.Rmd")), 
   "html_document", output_file="onlinecontent.html")
